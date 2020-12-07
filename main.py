9b3f6ee6cc2bb10f67c8d076a5cb2398f1702e55
Mert Serezli <mertserezli@gmail.com>
2020-12-07 12:28:16 +0300

    add minimal working program

---
+++ main.py
@@ -0,0 +1,30 @@
+import csv
+from collections import namedtuple
+
+
+def main():
+    file_name = input("Enter the q/a file name\n")
+    print("When a question is asked press enter if you know the answer, if not press n and enter")
+    read_qa = csv.reader(open(file_name), delimiter="\t")
+    QA = namedtuple('QA', "question answer")
+    qa_pairs = []
+    for row in read_qa:
+        qa_pairs.append(QA(*row))
+
+    while len(qa_pairs):
+        if ask(qa_pairs[0]):
+            qa_pairs.pop(0)
+        else:
+            qa_pairs.append(qa_pairs.pop(0))
+
+
+def ask(qa):
+    print(qa.question)
+    user_answer = input("Answer?\n")
+    if user_answer.lower().startswith('n'):
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    main()
